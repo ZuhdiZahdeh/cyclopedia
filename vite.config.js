07d576a3be653385e653d00537b2b9e9532ba065
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-06-21 09:14:17 +0300

    📝 تعديل على العنوان للتأكد من النشر التلقائي

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './', // مهم ليعمل الموقع بعد النشر على Firebase
-  build: {
-    outDir: 'dist', // المجلد الذي يتم نشره لاحقاً
-  }
-});
