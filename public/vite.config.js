ae913d74859c929c4ad90e995a8aac723c471e05
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-06-22 08:05:10 +0300

    تحديث صفحات animals وvegetables وتوحيد استدعاء JS عبر Vite

--- public/vite.config.js
+++ public/vite.config.js
@@ -7,12 +7,18 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         animals: resolve(__dirname, 'html/animals.html'),
+        birds: resolve(__dirname, 'html/birds.html'),
+        body: resolve(__dirname, 'html/body.html'),
+        colors: resolve(__dirname, 'html/colors.html'),
+        fruits: resolve(__dirname, 'html/fruits.html'),
+        transport: resolve(__dirname, 'html/transport.html'),
         vegetables: resolve(__dirname, 'html/vegetables.html')
       }
     },
     emptyOutDir: true
   }
 });
+
 
 
 
