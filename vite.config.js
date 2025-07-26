30dc4645e1768d938ce8f306baa024c6374db321
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-07-26 18:31:40 +0300

    🚀 إضافة صفحة المهن وربطها مع النظام العام

--- vite.config.js
+++ vite.config.js
@@ -12,20 +12,20 @@ export default defineConfig({
         // الصفحة الرئيسية
         main: resolve(__dirname, 'index.html'),
         // صفحات الموسوعة التعليمية
-		// animals: resolve(__dirname, 'cyclopedia-app/html/animals.html'),
-        // birds: resolve(__dirname, 'cyclopedia-app/html/birds.html'),
-        // body: resolve(__dirname, 'cyclopedia-app/html/body.html'),
-        // colors: resolve(__dirname, 'cyclopedia-app/html/colors-page.html'),
-        // fruits: resolve(__dirname, 'cyclopedia-app/html/fruits.html'),
-        // vegetables: resolve(__dirname, 'cyclopedia-app/html/vegetables.html'),
-        // humanBody: resolve(__dirname, 'cyclopedia-app/html/human-body.html'),
-        // plants: resolve(__dirname, 'cyclopedia-app/html/plants.html'),
-        // fish: resolve(__dirname, 'cyclopedia-app/html/fish.html'),
-        // transport: resolve(__dirname, 'cyclopedia-app/html/transport.html'),
-        // numbers: resolve(__dirname, 'cyclopedia-app/html/numbers.html'),
-        // time: resolve(__dirname, 'cyclopedia-app/html/time.html'),
+		// animals: resolve(__dirname, 'html/animals.html'),
+        // birds: resolve(__dirname, 'html/birds.html'),
+        // body: resolve(__dirname, 'html/body.html'),
+        // colors: resolve(__dirname, 'html/colors-page.html'),
+        // fruits: resolve(__dirname, 'html/fruits.html'),
+        // vegetables: resolve(__dirname, 'html/vegetables.html'),
+        // humanBody: resolve(__dirname, 'html/human-body.html'),
+        // plants: resolve(__dirname, 'html/plants.html'),
+        // fish: resolve(__dirname, 'html/fish.html'),
+        // transport: resolve(__dirname, 'html/transport.html'),
+        // numbers: resolve(__dirname, 'html/numbers.html'),
+        // time: resolve(__dirname, 'html/time.html'),
         // ✅ صفحة المهن الجديدة
-        professions: resolve(__dirname, 'cyclopedia-app/html/professions.html')
+        professions: resolve(__dirname, 'html/professions.html')
       }
     }
   }
