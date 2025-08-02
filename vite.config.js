3350df5087446f7298bb7d0772447129ca3eeee9
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-08-02 14:49:51 +0300

    update the Tools Match Game

--- vite.config.js
+++ vite.config.js
@@ -9,23 +9,10 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       input: {
-        // الصفحة الرئيسية
         main: resolve(__dirname, 'index.html'),
-        // صفحات الموسوعة التعليمية
-		// animals: resolve(__dirname, 'html/animals.html'),
-        // birds: resolve(__dirname, 'html/birds.html'),
-        // body: resolve(__dirname, 'html/body.html'),
-        // colors: resolve(__dirname, 'html/colors-page.html'),
-        // fruits: resolve(__dirname, 'html/fruits.html'),
-        // vegetables: resolve(__dirname, 'html/vegetables.html'),
-        // humanBody: resolve(__dirname, 'html/human-body.html'),
-        // plants: resolve(__dirname, 'html/plants.html'),
-        // fish: resolve(__dirname, 'html/fish.html'),
-        // transport: resolve(__dirname, 'html/transport.html'),
-        // numbers: resolve(__dirname, 'html/numbers.html'),
-        // time: resolve(__dirname, 'html/time.html'),
-		"tools-match": resolve(__dirname, 'html/tools-match.html'),
-        professions: resolve(__dirname, 'html/professions.html')
+        "tools-match": resolve(__dirname, 'html/tools-match.html'),
+        professions: resolve(__dirname, 'html/professions.html'),
+        toolsMatch: resolve(__dirname, 'src/js/tools-match-game.js') // ✅ أضف هذا السطر
       }
     }
   }
