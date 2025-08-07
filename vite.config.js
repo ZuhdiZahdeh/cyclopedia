dcb5d6bd195b4fc63cbd20cc75e3e8b1c723f4aa
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-08-07 19:48:10 +0300

    تحديث صفحة الخضروات النظام الجديد

--- vite.config.js
+++ vite.config.js
@@ -10,21 +10,19 @@ export default defineConfig({
       input: {
         // الصفحة الرئيسية
         main: resolve(__dirname, 'index.html'),
-		// ✅ أضف هذا السطر أو تأكد من صحته
-        mainjs: resolve(__dirname, 'src/main.js'),
-		
+
         // صفحات HTML للألعاب والمواضيع
         "tools-match": resolve(__dirname, 'html/tools-match.html'),
         "tools-match-controls": resolve(__dirname, 'html/tools-match-controls.html'),
         "professions": resolve(__dirname, 'html/professions.html'),
         "alphabet-press": resolve(__dirname, 'html/alphabet-press.html'),
         "vegetables": resolve(__dirname, 'html/vegetables.html'),
+        "fruits": resolve(__dirname, 'html/fruits.html'),
 
         // ملفات JavaScript المستقلة التي لا يتم تحميلها ديناميكيًا
         "tools-match-game": resolve(__dirname, 'src/js/tools-match-game.js'),
         "alphabet-press-game": resolve(__dirname, 'src/js/alphabet-press-game.js'),
         "include-all-scripts": resolve(__dirname, 'src/js/include-all-scripts.js'),
-		
 
         // لأغراض الاختبار أو التصحيح
         "debug": resolve(__dirname, 'html/debug.html')
