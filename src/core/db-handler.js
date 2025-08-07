2c7c8f8d4ac3f518a386425655950fbea8efaa30
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-08-07 11:58:28 +0300

    تحديث صفحة الخضروات النظام الجديد

--- src/core/db-handler.js
+++ src/core/db-handler.js
@@ -2,13 +2,12 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "./firebase-config.js";
+import { firebaseConfig } from "./firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db }; // ✅ هذا السطر هو المهم!
-
 
 export async function getItemsByCategory(category) {
   const colRef = collection(db, "categories", category, "items");
